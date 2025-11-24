c483cc2c3c9da2aafcdd2d4395955be55b096ca0
Silvi Sinanaj <silviberat@gmail.com>
2025-11-24 20:43:03 +0100

    Silvi (#3)
    * add: create index.html and index.js with JavaScript lesson content
    
    * js arrays, if conditions and loops
    
    * Lysien (#2)
    
    * push
    
    * push
    
    * wtf
    
    * Detyra done
    
    ---------
    
    Co-authored-by: Lysien <picarilysien795@gmail.com>
    
    ---------
    
    Co-authored-by: Lysien <picarilysien795@gmail.com>

---
+++ detyre.js
@@ -0,0 +1,25 @@
+// 14/11/2025
+// 1.link scriptin me html
+console.log("Lidhja u krye")
+// Deklaro tipe te ndryshme te datave (string, number, boolean, null, undefined)
+//your code here
+//console log verpime te ndryshme matematikore (+, -, *, /, %, ++, --)
+//your code here
+//string concatenation using different quotes (' ', " ", ` `)
+//your code here
+// template literals 
+// console log me template literals nje do jete me code ne linja te ndryshme
+//your code here
+// deklaro variable emer mbiemer  me germa te vogla 
+// do console log "Përshëndetje, unë jam [emer] [mbiemer]."
+
+// declaro variable per vitin e lindjes
+// declaro variable per moshen
+// do console log "Unë jam [mosha] vjeç dhe jam lindur në vitin [viti i lindjes]."
+
+
+// bej emrin dhe mbiemrin me shkronja te mdhaja 
+// do console log emrin e bere me shkronja te medha
+
+// deklaroji ne nje variabel tjeter emri-i-plote= emer (me germen e par kapitale )+ space + mbiemer (me germen e par kapitale)
+// do console log emri-i-plote. 
